8f47761e2af1ef296c097aa67405a8e28eec595f
Adrián <adrian.gaitan@utp.edu.co>
2023-08-20 00:35:32 -0500

    Point 3 done

--- 1.c
+++ 1.c
@@ -4,7 +4,7 @@
 - Versión del código: 1.2
 - Presentado por: Adrián Fernando Gaitán Londoño Ing(c)
 - Presentado a: Doctor, Ricardo Moreno Laverde
-- Lenguaje: C 
+- Lenguaje: C
 - Version del lenguaje: ISO/IEC 9899:2018 || C18
 - Compilador: Apple clang version 14.0.0 (clang-1400.0.28.1)
 - Universidad Tecnológica de Pereira
@@ -16,36 +16,38 @@
 - Nota: si el número no está en el rango, imprimir mensaje: No entregamos respuestas para valores fuera del rango
 */
 
+//Librerías
 #include <stdio.h>
 
-int main() {
+// Función principal
+int main()
+{
     // Declaración e inicialización de variables
-    int numero = 0;// Variable que almacena el número ingresado por el usuario
-    int primo = 0;// Variable que almacena si el número es primo o no
+    int numero = 0; // Variable que almacena el número ingresado por el usuario
+    int primo = 1;  // Variable que almacena si el número es primo o no
+    // Inicialmente arranca en indicando true, y si se encuentra que no es primo, se cambia a false (0)
 
-    //Mensaje de bienvenida
-
-    printf("Este programa, lee un número entre uno(1) y cinco(5), e imprime si es primo o no.\n");
-    printf("Ingrese un numero: ");
+    // Mensaje de bienvenida y solicitud de datos
+    printf("Este programa, lee un número entre uno(1) y cinco(5), e imprime si es primo o no.\nIngrese un número: ");
     scanf("%d", &numero);
-    
-    // Validación de si el número es primo o no
-    if (numero >= 0 && numero <= 5) {
-        if (numero == 2 || numero == 3 || numero == 5) {
-            primo = 1;
-        } else if (numero > 1 && numero % 2 != 0 && numero % 3 != 0 && numero % 5 != 0) {
-            primo = 1;
-        }
+
+    //Validación de que esté en el rango
+    if (! ( numero >= 1 && numero <= 5 ) ) {
+        printf("\nNo entregamos respuestas para valores fuera del rango");
+        return 0;// Finaliza el programa
     }
+
+    // Validación de si el número es primo o no
+    if ( numero == 1 || numero == 4 ) {
+        primo = 0;
+    } 
 
     // Impresión de resultados
-    if (primo) {
-        printf("\nEl numero %d es primo", numero);
-    } else if (numero >= 0 && numero <= 5) {
-        printf("\nEl numero %d no es primo", numero);
+    if (primo == 1) {
+        printf("\nEl número es primo");
     } else {
-        printf("\nNo entregamos respuestas para valores fuera del rango");
+        printf("\nEl número no es primo");
     }
-    
+
     return 0;
 }
---
+++ 3.c
@@ -0,0 +1,41 @@
+/*
+- Fecha: 12 de agosto 2023
+- Hora: 12:00
+- Versión del código: 1.2
+- Presentado por: Adrián Fernando Gaitán Londoño Ing(c)
+- Presentado a: Doctor, Ricardo Moreno Laverde
+- Lenguaje: C
+- Version del lenguaje: ISO/IEC 9899:2018 || C18
+- Compilador: Apple clang version 14.0.0 (clang-1400.0.28.1)
+- Universidad Tecnológica de Pereira
+- Programa de Ingeniería de Sistemas y Computación
+- El programa recibe como valor de entrada un número entre 1 y 7, y muestra un mensaje en pantalla con el día de la semana correspondiente.
+- Entrada: Un número entre 1 y 7
+- Salida: Un mensaje que indica el día de la semana correspondiente
+- Restricciones: El rango en que identifica el día de la semana es de 1 a 7
+*/
+
+//Librerías
+#include <stdio.h>
+
+// Función principal
+int main () {
+    // Arreglo de días de la semana
+    char *dias[7] = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
+    int dia = 0;// Variable que almacena el número ingresado por el usuario
+
+    // Mensaje de bienvenida y solicitud de datos
+    printf("Este programa, lee un número entre [1 y 7]\ne imprime el día que le corresponde en la semana.\nEntre un número: ");
+    scanf("%d", &dia);
+
+    //Validación de que esté en el rango
+    if ( !(dia >= 1 && dia <= 7) ) {
+        printf("El número ingresado no está en el rango");
+        return 0;// Finaliza el programa
+    } 
+
+    // Impresión de resultados
+    printf("El día es %s", dias[dia - 1]);
+
+    return 0;
+}
