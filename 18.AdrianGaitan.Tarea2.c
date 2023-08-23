757c64d5ab7df454083142115ee8f6918fe601cf
Adrián <adrian.gaitan@utp.edu.co>
2023-08-22 23:55:35 -0500

    Point 19 done

---
+++ 18.AdrianGaitan.Tarea2.c
@@ -0,0 +1,53 @@
+/*
+- Fecha: 12 de agosto 2023
+- Hora: 12:00
+- Versión del código: 1.2
+- Presentado por: Adrián Fernando Gaitán Londoño Ing(c)
+- Presentado a: Doctor, Ricardo Moreno Laverde
+- Lenguaje: C
+- Version del lenguaje: ISO/IEC 9899:2018 || C18
+- Compilador: Apple clang version 14.0.0 (clang-1400.0.28.1)
+- Universidad Tecnológica de Pereira
+- Programa de Ingeniería de Sistemas y Computación
+- Este programa fue desarrollado para un establecimiento de parqueadero.
+- Toma como valor de entrada tres valores, placa del vehículo, el número de horas y minutos que estuvo en el parqueadero.
+- El precio de la hora o fracció es de: 1800
+- Entrada: 1 string y dos números
+- Salida: se imprime en pantalla el valor a pagar
+*/
+// Librerías
+#include <stdio.h>
+
+// Función principal
+int main () {
+    // Declaración e inicialización de variables 
+    char placa[7]; // Variable que almacena la placa del vehículo
+    int horas = 0, minutos = 0; // Variables que almacenan el número de horas y minutos que estuvo el vehículo en el parqueadero 
+    float pago = 0; // Variable que almacena el valor a pagar
+
+    // Mensaje de bienvenida y solicitud de datos
+    printf("Ingresa la placa del vehículo: ");
+    scanf("%s", placa);
+    printf("Digite el número de horas en el parqueadero: ");
+    scanf("%d", &horas);
+    printf("Digite el número de minutos en el parqueadero: ");
+    scanf("%d", &minutos);
+
+    // Calculo del valor a pagar
+    pago = horas * 1800;
+
+    if ( minutos >= 0) {
+        if ( minutos > 60 ) {
+            if ( (minutos % 60) >= 1) {
+                pago = pago + (minutos / 60) * 1800 + 1800;
+            } else {
+                pago = pago + 1800;
+            }
+        } else {
+            pago = pago + 1800;
+        }
+    }
+
+    // Impresión de resultados
+    printf("El valor a pagar es: \x1b[32m%.2f\x1b[0m", pago);
+}
---
+++ 19.AdrianGaitan.Tarea2.c
@@ -0,0 +1,48 @@
+/*
+- Fecha: 12 de agosto 2023
+- Hora: 12:00
+- Versión del código: 1.2
+- Presentado por: Adrián Fernando Gaitán Londoño Ing(c)
+- Presentado a: Doctor, Ricardo Moreno Laverde
+- Lenguaje: C
+- Version del lenguaje: ISO/IEC 9899:2018 || C18
+- Compilador: Apple clang version 14.0.0 (clang-1400.0.28.1)
+- Universidad Tecnológica de Pereira
+- Programa de Ingeniería de Sistemas y Computación
+- El programa toma como valor de entrada el número de docenas de mouses que desea adquirir el cliente.
+- La tienda se encuentra en promoción. Por la compra de más de 3 docenas recibe un descuento del 15%.
+- Además se obsequia un mouse por docena que supere la cifra de 3, es decir, si el cliente compro más de 3 docenas,
+- las docenas adicionales reciben el obsequio de un mouse por docena. Precio cada mouse: 15$
+- Entrada: un número
+- Salida: se imprime el valor de la compra original, el descuento, unidades obsequiadas y el valor a pagar
+*/
+// Librerías
+#include <stdio.h>
+
+// Función principal
+int main () {
+    // Declaración e inicialización de variables
+    int docenas = 0, obsequio = 0; // Variables que almacenan el número de docenas y unidades de mouses
+    float valor = 15, descuento = 0, montoCompra, montoFinal = 0; // Variables que almacenan el valor de la compra, el descuento y el monto final
+
+    // Mensaje de bienvenida y solicitud de datos
+    printf("Este mes los mouse están en descuento por la compra\nde más de 3 docenas recibe un descuento del 15%%.\nAdicional por cada docena llevas un mouse gratis,\nsolo aplica para las docenas excedentes a las 3 primeras.\nLas compras inferiores reciben 10%% de descuento.");  
+    printf("\n\nPrecio c/u: 15$\n\nIngresa el número de docenas que deseas comprar: ");
+    scanf("%d", &docenas);
+
+    // Calculo del valor de la compra
+    montoCompra = docenas * 12 * valor;
+
+    // Calculo del descuento
+    if ( docenas > 3 ) {
+        obsequio = (docenas - 3);
+        descuento = montoCompra * 0.15 + (obsequio * valor);
+        montoFinal = montoCompra - descuento;        
+    } else {
+        descuento = montoCompra * 0.1;
+        montoFinal = montoCompra - descuento;
+    }
+
+    // Impresión de resultados
+    printf("\nMonto de compra: %.2f$\nDescuento: %.2f$\nUnidades obsequiadas: %d\nMonto final: \x1b[32m%.2f$\x1b[0m", montoCompra, descuento, obsequio, montoFinal);
+}
